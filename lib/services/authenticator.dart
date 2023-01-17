71297ef5638cf00e29065745ff88880b6c819380
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2023-01-17 18:03:15 +0100

    remove singletone

--- lib/services/authenticator.dart
+++ lib/services/authenticator.dart
@@ -1,25 +1,24 @@
 import '../models/login.dart';
 
 class AuthenticatorService {
-  AuthenticatorService._();
-  static final AuthenticatorService _authenticatorService =
-      AuthenticatorService._();
-  static AuthenticatorService get instance => _authenticatorService;
+  static Login user = Login.empty;
 
-  //
-  Future<Login> createUser(String email, String password) async {
+  static Future<Login> createUser(String email, String password) async {
     // check login being stored on device
     var login = Login.empty;
 
-    if (Login.isEmpty(login)) {
-      var cloud = await createOneDriveToken(email, password);
+    if (!Login.exists(login)) {
+      var cloud = await _createOneDriveToken(email, password);
       login = Login(email, password, cloud);
     }
+
+    user = login;
 
     return login;
   }
 
-  Future<String> createOneDriveToken(String email, String password) async {
+  static Future<String> _createOneDriveToken(
+      String email, String password) async {
     return Future.sync(() => 'onedrivetokenonedrivekeytokenkey');
   }
 }
